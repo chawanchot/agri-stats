db4aead5d064a4006b108cb900548fa2dd9b125f
chawanchot <chawanchot.dev@gmail.com>
2026-01-08 10:38:18 +0700

    [Add]: router

---
+++ src/router/MainRouter.tsx
@@ -0,0 +1,19 @@
+import HomePage from "@pages/HomePage";
+import MainPage from "@pages/MainPage";
+import { useRoutes } from "react-router-dom";
+
+const MainRouter = () => {
+    return useRoutes([
+        {
+            path: "/",
+            element: <MainPage />,
+        },
+        {
+            path: "/home",
+            element: <HomePage />,
+            
+        }
+    ]);
+};
+
+export default MainRouter;
--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
             "@components": path.resolve(__dirname, "src/components"),
             "@assets": path.resolve(__dirname, "src/assets"),
             "@data": path.resolve(__dirname, "src/data"),
+            "@router": path.resolve(__dirname, "src/router"),
         },
     },
     server: {
