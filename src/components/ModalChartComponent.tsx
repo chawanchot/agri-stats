e01692ad3396e0b1fd740d3ebbb0b4c07645d6ef
Aunchana Kongmanee <artozone555@gmail.com>
2026-01-14 16:28:47 +0700

    [Edit]: modal scroll and chart

--- src/components/ModalChartComponent.tsx
+++ src/components/ModalChartComponent.tsx
@@ -2,66 +2,68 @@ import type { ApexOptions } from "apexcharts";
 import Chart from "react-apexcharts";
 
 type PropsType = {
-    data: CropType[];
+  data: CropType[];
 };
 
 type CropType = {
-    name: string;
-    data: CropDetailType[];
+  name: string;
+  data: CropDetailType[];
 };
 
 type CropDetailType = {
-    crop: string;
-    harvest_area: number;
-    planted_area: number;
-    province: string;
-    year: number;
-    yield_per_rai: number;
-    yield_ton: number;
+  crop: string;
+  harvest_area: number;
+  planted_area: number;
+  province: string;
+  year: number;
+  yield_per_rai: number;
+  yield_ton: number;
 };
 
 function ModalChartComponent({ data }: PropsType) {
-    console.log(data);
+  console.log(data);
 
-    const chartOptions: ApexOptions = {
-        chart: {
-            toolbar: {
-                show: false,
-            },
-        },
-        dataLabels: {
-            enabled: false,
-        },
-        grid: {
-            row: {
-                colors: ["#f3f3f3", "transparent"],
-                opacity: 0.5,
-            },
-        },
-        xaxis: {
-            categories: ["2563", "2564", "2565", "2566", "2567"],
-        },
-    }
+  const categories = Array.from(
+    new Set(
+      data.flatMap((cropGroup) => cropGroup.data.map((item) => item.year))
+    )
+  ).sort((a, b) => a - b);
 
-    const chartData = [
-        {
-            name: "ทุเรียน",
-            data: [202, 245, 275, 295, 325],
-        },
-        {
-            name: "ยางพารา",
-            data: [252, 285, 300, 312, 335],
-        },
-    ];
+  const chartData = data.map((cropGroup) => ({
+    name: cropGroup.name,
+    data: categories.map((year) => {
+      const yearlyRecord = cropGroup.data.find((item) => item.year === year);
 
-    return (
-        <Chart
-            options={chartOptions}
-            series={chartData}
-            type="line"
-            height={250}
-        />
-    );
+      return yearlyRecord ? yearlyRecord.yield_per_rai : 0;
+    }),
+  }));
+
+  const chartOptions: ApexOptions = {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    grid: {
+      row: {
+        colors: ["#f3f3f3", "transparent"],
+        opacity: 0.5,
+      },
+    },
+    xaxis: {
+      categories: categories.map(String),
+    },
+    tooltip: {
+      enabled: false
+    },
+  };
+
+  return (
+    <Chart options={chartOptions} series={chartData} type="line" height={250} />
+  );
 }
 
 export default ModalChartComponent;
